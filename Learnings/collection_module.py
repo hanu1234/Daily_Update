324d1d52f754872b270689399576fe7cefbc96fc
Hanamanta Shivanagi <hshivanagi@extremenetworks.com>
2019-12-10 10:27:08 +0530

    Adding Colllection Module
    adding different type of collection modules

---
+++ Learnings/collection_module.py
@@ -0,0 +1,16 @@
+"""
+python collection module is use to improve the functionality of the built-in collection container.
+python built in collection containers are.
+1. list
+2. tuple
+3. dect
+4. set
+
+Below are the collection module.
+1. Counter
+2. defaultdict
+3. OrderdDict
+4. deque
+5. ChainMap
+6. namedtuple
+"""
