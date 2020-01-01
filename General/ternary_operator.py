c4dea14faf62712872540570730359c8d21e1e93
Hanamanta Shivanagi <hshivanagi@extremenetworks.com>
2020-01-01 16:28:37 +0530

    some changes

---
+++ General/ternary_operator.py
@@ -0,0 +1,21 @@
+"""
+condition ? value_if_true : value_if_false   basic ternary operator syntax
+IN python
+
+[on_true] if [expression] else [on_false]
+"""
+
+
+# Python program to demonstrate nested ternary operator
+a, b = 10, 20
+
+print ("Both a and b are equal" if a == b else "a is greater than b" if a > b else "b is greater than a")
+
+
+# Program to demonstrate conditional operator
+a, b = 10, 20
+
+# Copy value of a in min if a < b else copy b
+min = a if a < b else b
+print(min)
+
