51b2725cd938767b8559ca49d1e8de56ac307585
hshivanagi <hshivanagi@extremenetworks.com>
2020-03-19 08:52:21 +0530

    Second changes

--- XML_Parsing/xml_parsing_ex1.py
+++ XML_Parsing/xml_parsing_ex1.py
@@ -38,6 +38,7 @@ for x in root[msm_index]:
 
 tree.write('Open_Auth2.xml', default_namespace='xmlns')
 print("Hanamant Testing")
+print("Hanamnat Testing2")
 
 def modify_xml(ssid):
     for x in root:
