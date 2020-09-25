0313c693263352a12511dc9f760317a2474b0349
hshivanagi <hshivanagi@extremenetworks.com>
2020-09-25 14:41:46 +0530

    decorator

--- Share_Market/GetNseTrend.py
+++ Share_Market/GetNseTrend.py
@@ -1,5 +1,4 @@
 import requests
-import re
 from bs4 import BeautifulSoup
 
 
@@ -10,7 +9,21 @@ class NseTrend(object):
     def get_trend(self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, "html.parser")
-        data_array = soup.find(id="nlp").getText().strip().split()[-1]
-        trend = re.search(r'[-0123456]\d*.\d*', data_array)
-        perc_trend = trend.group()
-        return perc_trend
+        divs = soup.findAll("table", {"class": "mctable1"})
+        nifty_trend = {}
+        for div in divs:
+            rows = div.findAll('tr')
+            for row in rows:
+                if "NIFTY 50" in row.text:
+                    column = row.findAll("td")
+                    nifty_trend['index'] = column[0].text
+                    nifty_trend['Price'] = column[1].text
+                    nifty_trend['Change'] = column[2].text
+                    nifty_trend['%Chg'] = column[3].text
+                    break
+        print(nifty_trend)
+
+
+if __name__ == "__main__":
+    obj = NseTrend()
+    obj.get_trend()
