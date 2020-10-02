bb6f5a5d2b86d80ee5b733fbea39d4b5e2b92518
hshivanagi <hshivanagi@extremenetworks.com>
2020-10-02 14:47:06 +0530

    decorator

--- Share_Market/live_top_gainers.py
+++ Share_Market/live_top_gainers.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 
-class Live(object):
+class LiveGainers(object):
     def __init__(self):
         self.url = "https://www.moneycontrol.com/stocks/marketstats/nsegainer/index.php"
 
@@ -11,31 +11,32 @@ class Live(object):
         pd.set_option('display.max_columns', None)
         df_list = pd.read_html(response)
         df = df_list[0]
-        top_gainer_data = {'company_names': [], 'High': [], 'Low': [], 'Gain': [], 'Last_Price': [], 'Prev_Close': []}
+        print(df.columns)
+        live_data = {'company_names': [], 'High': [], 'Low': [], 'Gain': [], 'Last_Price': [], 'Prev_Close': []}
 
         company_name = df['Company Name'][0::7]
         for company in company_name:
-            top_gainer_data['company_names'].append(" ".join(company.split()[0:2]))
+            live_data['company_names'].append(" ".join(company.split()[0:2]))
 
         for high in df['High'][0::7]:
-            top_gainer_data['High'].append(high)
+            live_data['High'].append(high)
 
         for low in df['Low'][0::7]:
-            top_gainer_data['Low'].append(low)
+            live_data['Low'].append(low)
 
         for gain in df['% Gain'][0::7]:
-            top_gainer_data['Gain'].append(gain)
+            live_data['Gain'].append(gain)
 
         for last_price in df['Last Price'][0::7]:
-            top_gainer_data['Last_Price'].append(last_price)
+            live_data['Last_Price'].append(last_price)
 
         for close in df['Prev Close'][0::7]:
-            top_gainer_data['Prev_Close'].append(close)
+            live_data['Prev_Close'].append(close)
 
-        df = pd.DataFrame(top_gainer_data)
+        df = pd.DataFrame(live_data)
         print(df)
 
 
 if __name__ == "__main__":
-    obj = Live()
+    obj = LiveGainers()
     obj.get_live_data()
---
+++ Share_Market/live_top_loosers.py
@@ -0,0 +1,42 @@
+import requests
+import pandas as pd
+
+
+class LiveLoosers(object):
+    def __init__(self):
+        self.url = "https://www.moneycontrol.com/stocks/marketstats/nseloser/index.php"
+
+    def get_live_data(self):
+        response = requests.get(self.url).content
+        pd.set_option('display.max_columns', None)
+        df_list = pd.read_html(response)
+        df = df_list[0]
+        print(df.columns)
+        live_data = {'company_names': [], 'High': [], 'Low': [], 'Loss': [], 'Last_Price': [], 'Prev_Close': []}
+
+        company_name = df['Company Name'][0::7]
+        for company in company_name:
+            live_data['company_names'].append(" ".join(company.split()[0:2]))
+
+        for high in df['High'][0::7]:
+            live_data['High'].append(high)
+
+        for low in df['Low'][0::7]:
+            live_data['Low'].append(low)
+
+        for gain in df['% Loss'][0::7]:
+            live_data['Loss'].append(gain)
+
+        for last_price in df['Last Price'][0::7]:
+            live_data['Last_Price'].append(last_price)
+
+        for close in df['Prev Close'][0::7]:
+            live_data['Prev_Close'].append(close)
+
+        df = pd.DataFrame(live_data)
+        print(df)
+
+
+if __name__ == "__main__":
+    obj = LiveLoosers()
+    obj.get_live_data()
